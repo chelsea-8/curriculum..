bfa108cecabee12e06e0d11d9ec7b6270021e3e9
chelsea-8 <chelsea.102321@gmail.com>
2021-08-26 21:26:39 +0900

    1-4.5
    課題

--- JavaStudy/Check1.java
+++ JavaStudy/Check1.java
@@ -21,20 +21,20 @@ public class Check{
 			* 問2
 			* 以下のfor文を完成させなさい
 			*/
-			for(int i = 0; i < 7; i++){
+			for(int i = 0; i < data.length; i++){
 					System.out.print(data[i] + " ");
 			}
 			System.out.println();
 			for (int i = 0; i < data.length; i++) {
-					for (int j = data.length; j > i; j--) {
+					for (int j = data.length - 1; j > i; j--) {
 							/*
 							* 問3
 							* 以下、配列の添字を入れてソートを完成させなさい
 							*/
-							if(data[/*ここに記述*/] > data[/*ここに記述*/]){
-								int box = data[/*ここに記述*/];
-								data[/*ここに記述*/] = data[/*ここに記述*/];
-								data[/*ここに記述*/] = box;
+							if(data[i] > data[j]){
+								int box = data[i];
+								data[i] = data[j];
+								data[j] = box;
 							}
 					}
 			}
