c1341ca05cd9db95e2dcaf8f368e779475a8ab78
chelsea-8 <chelsea.102321@gmail.com>
2021-10-21 22:10:43 +0900

    1021
    1021

--- JavaStudy/Task1_4.java
+++ JavaStudy/Task1_4.java
@@ -34,7 +34,7 @@ public class Task1_4 {
 
 		// ② 「name」の値が「USER_NAME」と等しく、「 pass 」の値が「USER_PASSWORD」と等しい場合。
 		// 定数を使用して「 ログイン成功です 」と出力してください。
-		else if (USER_NAME.equals(name) && USER_PASSWORD.equals(pass)) {
+		 if (USER_NAME.equals(name) && USER_PASSWORD.equals(pass)) {
 			System.out.println(CONST_MSG_SUCCESS);
 		}
 
@@ -51,7 +51,9 @@ public class Task1_4 {
 		
 		// ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。
 		// 定数を使用して「 入力情報に誤りがあります。 」と出力してください。
-		else (!USER_NAME.equals("") && !USER_PASSWORD.equals("")) {
+		else 
+		// (!USER_NAME.equals("") && !USER_PASSWORD.equals("")) 
+		{
 			System.out.println(CONST_MSG_ERROR_INPUT);
 		}
 	}
