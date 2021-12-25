3f0ff0ccd94ae36e520f7dd488460c8301682474
chelsea-8 <chelsea.102321@gmail.com>
2021-12-25 14:28:12 +0900

    4-3
    4-3

--- 4-3/sqlTask.sql
+++ 4-3/sqlTask.sql
@@ -12,21 +12,12 @@
  
 -- 3. 在庫テーブルに店舗テーブル、商品テーブルを「内部結合」し、店舗名・商品名・在庫数を全て取得しなさい。
 -- [回答]
---  SELECT C.store_name, D.goods_name, C.quantity 
---  FROM store_table 
---  INNER JOIN stock_table as B ON A.store_code = B.store_code; 
---  INNER JOIN goods_table as D ON D.goods_code = C.goods_code;
-
-
-SELECT C.store_name, D.goods_name, C.quantity 
- FROM stock_table as A
- 
- INNER JOIN store_table as B 
-   ON A.store_code = B.store_code; 
- 
- INNER JOIN goods_table as D 
-   ON A.goods_code = D.goods_code;
-
+SELECT B.store_name, C.goods_name, A.quantity 
+FROM stock_table A
+INNER JOIN store_table B
+ON A.store_code = B.store_code
+INNER JOIN goods_table C
+ON A.goods_code = C.goods_code
 
 
 
@@ -50,15 +41,15 @@ SELECT C.store_name, D.goods_name, C.quantity
 -- [回答]
  INSERT 
 INTO goods_table(goods_code, goods_name, price, update_day) 
-VALUES ('M001', '商品名= マフラー', 4500, CURDATE());
+VALUES ('M001', '商品名= マフラー', 4500,  CURRENT_DATE());
  
 -- 7. 在庫テーブルの商品コード='S987'、かつ、店舗コード='EA01'に対して、「在庫数=10、更新日付=本日日付」で更新しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
-UPDATE stock_table SET quantity = 10 , update_day = CURDATE()
+UPDATE stock_table SET quantity = 10 , update_day = CURRENT_DATE()
 WHERE goods_code = 'S987' AND store_code ='EA01';
  
 -- 8. 7で更新した商品を削除しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
-DELETEFROM stock_table WHERE goods_code = 'S987' AND store_code ='EA01';
+DELETE FROM stock_table WHERE goods_code = 'S987' AND store_code ='EA01';
 
 
